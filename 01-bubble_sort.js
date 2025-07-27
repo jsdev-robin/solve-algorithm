af86065d2836796cacde0207df3e655ce61c8a20
Md Robin <jsdev.robin@gmail.com>
2025-07-27 21:32:36 +0600

    Done bubble sort

---
+++ 01-bubble_sort.js
@@ -0,0 +1,16 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+const arr = [34, 12, 87, 45, 23, 67, 3, 91, 10, 56];
+
+console.log(bubbleSort(arr));
