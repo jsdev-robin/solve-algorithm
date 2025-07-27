f168f2d438e29e0e844b2ea03bff1757acc498a8
Md Robin <jsdev.robin@gmail.com>
2025-07-27 22:44:11 +0600

    Insertion sort done

--- 01-bubble_sort.js
+++ 01-bubble_sort.js
@@ -8,6 +8,7 @@ function bubbleSort(arr) {
       }
     }
   }
+
   return arr;
 }
 
---
+++ 02-insertion_sort.js
@@ -0,0 +1,19 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    const key = arr[i];
+    const j = i - 1;
+
+    while (i >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+
+    arr[j + 1] = key;
+  }
+
+  return arr;
+}
+
+const arr = [34, 12, 87, 45, 23, 67, 3, 91, 10, 56];
+
+console.log(insertionSort(arr));
