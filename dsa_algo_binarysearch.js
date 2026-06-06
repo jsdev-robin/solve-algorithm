bbb909b577db16a70399963fe5c42dc224d0399a
jsdev-robin <jsdev.robin@gmail.com>
2026-06-06 22:44:20 +0600

    implement bubble sort algorithm and add additional sorting algorithms; update package scripts

---
+++ dsa_algo_binarysearch.js
@@ -0,0 +1,7 @@
+function binarysearch(arr) {
+  const n = arr.length;
+
+  return arr;
+}
+
+console.log(binarysearch([3, 20, 1, 6, 8, 2, 9, 1, 10, 9, 7]));
---
+++ dsa_algo_quicksort.js
@@ -0,0 +1,7 @@
+function quicksort(arr) {
+  const n = arr.length;
+
+  return arr;
+}
+
+console.log(quicksort([3, 20, 1, 6, 8, 2, 9, 1, 10, 9, 7]));
