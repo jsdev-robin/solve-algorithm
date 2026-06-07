82eeb80645b923f56bc72bd03a520c52ad689728
jsdev-robin <jsdev.robin@gmail.com>
2026-06-08 00:26:51 +0600

    feat: implement removeDuplicates function to remove duplicates from a sorted array

--- two-pointers.js
+++ two-pointers.js
@@ -69,3 +69,25 @@ function reverseArray(nums) {
 }
 
 console.log(reverseArray([5, 2, 3, 4, 1]));
+
+console.log(
+  '=====================removeDuplicates===============================',
+);
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+function removeDuplicates(nums) {
+  let slow = 0;
+
+  for (let fast = 1; fast < nums.length; fast++) {
+    if (nums[fast] !== nums[slow]) {
+      slow++;
+      nums[slow] = nums[fast];
+    }
+  }
+
+  return slow + 1;
+}
+
+console.log(removeDuplicates([1, 1, 2, 3, 3]));
