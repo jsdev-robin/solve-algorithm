df45f2fb03d87ef7248a5c01b3c2af67a89663e7
jsdev-robin <jsdev.robin@gmail.com>
2026-06-30 23:43:49 +0600

    feat: maxProfit

---
+++ array_maxProfit.js
@@ -0,0 +1,13 @@
+function maxProfit(prices) {
+  let minPrice = Infinity;
+  let profit = 0;
+
+  for (let p of prices) {
+    minPrice = Math.min(minPrice, p);
+    profit = Math.max(profit, p - minPrice);
+  }
+
+  return profit;
+}
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
