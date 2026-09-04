56780e2870e241564cc22c1dd175300ffe7b9cb0
jsdev-robin <jsdev.robin@gmail.com>
2026-09-04 11:28:45 +0600

    feat: implement linked list class with various methods for manipulation

--- index.js
+++ index.js
@@ -1,7 +1,89 @@
-const nums1 = [1, 2, 2, 1];
-const map = new Map();
-for (const n of nums1) {
-  map.set(n, (map.get(n) || 0) + 1);
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
 }
 
-console.log(map);
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  printList() {
+    let curr = this.head;
+    let listStr = '';
+
+    while (curr) {
+      listStr += curr.data + ' -> ';
+      curr = curr.next;
+    }
+
+    listStr += 'null';
+    console.log(listStr);
+  }
+
+  append(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let curr = this.head;
+      while (curr.next) {
+        curr = curr.next;
+      }
+      curr.next = newNode;
+      this.size++;
+    }
+  }
+
+  prepend(data) {
+    let newNode = new Node(data);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.size++;
+  }
+
+  insertAt(data, index) {
+    if (index < 0 || index > this.size) {
+      console.log('Invalid index');
+      return;
+    }
+
+    if (index === 0) {
+      this.prepend(data);
+      return;
+    }
+
+    let newNode = new Node(data);
+    let curr = this.head;
+    let prev = null;
+    let i = 0;
+
+    while (i < index) {
+      prev = curr;
+      curr = curr.next;
+      i++;
+    }
+
+    newNode.next = curr;
+    prev.next = newNode;
+    this.size++;
+  }
+}
+const list = new LinkedList();
+
+list.append(10);
+list.append(20);
+list.append(30);
+list.append(40);
+list.append(50);
+list.prepend(5);
+list.insertAt(60, 5);
+
+list.printList();
